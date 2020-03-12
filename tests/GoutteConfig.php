2586719821f80964b6368f693d9049fca554752d
Alexander Obuhovich <aik.bold@gmail.com>
2020-03-12 11:12:53 +0200

    Force PHP 5.6 for web server only on PHP 5.3

--- tests/GoutteConfig.php
+++ tests/GoutteConfig.php
@@ -23,4 +23,22 @@ class GoutteConfig extends AbstractConfig
     {
         return false;
     }
+
+    public function skipMessage($testCase, $test)
+    {
+        if (
+            'Behat\Mink\Tests\Driver\Form\Html5Test' === $testCase
+            && in_array($test, array(
+                'testHtml5FormAction',
+                'testHtml5FormMethod',
+            ))
+            && !method_exists('Symfony\Component\DomCrawler\Tests\FormTest', 'testGetMethodWithOverride')
+            // Symfony 4.4 removed tests from dist archives, making the previous detection return a false-positive
+            && !method_exists('Symfony\Component\DomCrawler\Form', 'getName')
+        ) {
+            return 'Mink Goutte doesn\'t support HTML5 form attributes before Symfony 3.3';
+        }
+
+        return parent::skipMessage($testCase, $test);
+    }
 }
