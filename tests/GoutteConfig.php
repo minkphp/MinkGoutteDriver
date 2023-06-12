84b804abe6df84eeef47de93210533f593b20fd5
Christophe Coevoet <stof@notk.org>
2023-06-12 11:24:34 +0200

    Skip unsupported tests

--- tests/GoutteConfig.php
+++ tests/GoutteConfig.php
@@ -3,6 +3,8 @@
 namespace Behat\Mink\Tests\Driver;
 
 use Behat\Mink\Driver\GoutteDriver;
+use Behat\Mink\Tests\Driver\Basic\IFrameTest;
+use Behat\Mink\Tests\Driver\Basic\ScreenshotTest;
 
 class GoutteConfig extends AbstractConfig
 {
@@ -17,6 +19,19 @@ class GoutteConfig extends AbstractConfig
     public function createDriver()
     {
         return new GoutteDriver();
+    }
+
+    public function skipMessage($testCase, $test): ?string
+    {
+        if ($testCase === IFrameTest::class) {
+            return 'iFrames management is not supported.';
+        }
+
+        if ($testCase === ScreenshotTest::class) {
+            return 'Screenshots are not supported.';
+        }
+
+        return parent::skipMessage($testCase, $test);
     }
 
     protected function supportsJs()
